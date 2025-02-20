1b69969e04600f64e42e693a1964bff3230d274a
LABP1KOMP <LABP1KOMP@LAB-P1-PC20>
2025-02-20 16:19:27 +0700

    ProjectSatu

---
+++ src/KEYSA200225/AritmatikaSatu.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package KEYSA200225;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class AritmatikaSatu {
+     public static void main(String[] args)
+     {
+         int i = 37, j = 42;
+         double x = 27.475, y = 7.22;
+         
+         //sedikit angka
+         System.out.println("Variabel values...");
+         System.out.println(" i = " + i);
+         System.out.println(" j = " + j);
+         System.out.println(" x = " + x);
+         System.out.println(" y = " + y);
+         
+         //penjumlahan angka 
+         System.out.println("Adding...");
+         System.out.println(" i + j = " + (i + j));
+         System.out.println(" x + y = " + (x + y));
+         
+         //pengurangan angka
+         System.out.println("Subtracting...");
+         System.out.println(" i - j = " + (i - j));
+         System.out.println(" x - y = " + (x - y));
+         
+     }
+}
