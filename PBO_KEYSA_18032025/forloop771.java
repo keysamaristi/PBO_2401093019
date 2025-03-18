814919cef82ec565ea47dbc83dd967c67c344599
keysamaristi <keysamaristi01@gmail.com>
2025-03-18 10:01:24 +0700

    18032025

---
+++ PBO_KEYSA_18032025/forloop771.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class forloop771 {
+    public static void main(String[] args) {
+        String days[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        System.out.println("\nUsing for-loop:");
+        for (int k = 0; k < days.length; k++) {
+            System.out.println(days[k]);
+        }
+    }
+}
